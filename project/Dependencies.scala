4d24f7c781c84f34ebe956d1100b261b99a3cbc3
Scala Steward <dev-berlin@liveintent.com>
2026-04-14 09:39:59 +0000

    Berlin Steward can update zio-aws-cloudwatch from 7.42.28.1 to 7.42.28.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     val jaxB                      = "2.3.1"
     val kcl                       = "3.4.1"
     val zio                       = "2.1.25"
-    val zioAws                    = "7.42.28.1"
+    val zioAws                    = "7.42.28.3"
     val zioInteropReactiveStreams = "2.0.2"
     val zioLogging                = "2.5.3"
   }
