48f560a7266f67ad07fab1bd9fcce7f23ab863e1
Scala Steward <dev-berlin@liveintent.com>
2026-05-12 02:51:06 +0000

    Berlin Steward can update zio from 2.1.25 to 2.1.26

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
     val hdrHistogram              = "2.2.2"
     val jaxB                      = "2.3.1"
     val kcl                       = "3.4.3"
-    val zio                       = "2.1.25"
+    val zio                       = "2.1.26"
     val zioAws                    = "7.42.28.3"
     val zioInteropReactiveStreams = "2.0.2"
     val zioLogging                = "2.5.3"
